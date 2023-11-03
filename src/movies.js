f368210901b4983d37b75ba4c6952a3a2b587aed
Munisakhon Ermatova <munisakhonermatova@munisakhons-air.speedport.ip>
2023-11-03 19:49:53 +0100

    still in progress

--- src/movies.js
+++ src/movies.js
@@ -1,19 +1,60 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+  const newListOfDirectors = moviesArray.map((film) => film.director);
+  return newListOfDirectors;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
-
+function howManyMovies(moviesArray) {
+  const spielbergDramas = moviesArray.filter((movies) => {
+    return (
+      movies.director === "Steven Spielberg" && movies.genre.includes("Drama")
+    );
+  });
+  return spielbergDramas.length;
+}
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  if (movies.length === 0) {
+    return 0;
+  }
+
+  const totalScore = movies.reduce((accumulator, movie) => {
+    if (movie.score !== undefined) {
+      return accumulator + movie.score;
+    } else {
+      return accumulator;
+    }
+  }, 0);
+
+  const averageScore = totalScore / movies.length;
+
+  return parseFloat(averageScore.toFixed(2));
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  const dramaMovies = movies.filter((movie) => movie.genre.includes("Drama"));
+
+  if (dramaMovies.length === 0) {
+    return 0;
+  }
+
+  const totalScore = dramaMovies.reduce((accumulator, movie) => {
+    return accumulator + movie.score;
+  }, 0);
+
+  const averageScore = totalScore / dramaMovies.length;
+
+  return parseFloat(averageScore.toFixed(2));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  const moviesCopy = movies.slice();
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(moviesArray) {}
